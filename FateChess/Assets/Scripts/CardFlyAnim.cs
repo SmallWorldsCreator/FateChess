4d1739ad39991789c2ef1e584c909196d0f86ed4
danny19940914 <38798778+danny19940914@users.noreply.github.com>
2018-04-28 21:41:17 +0800

    UI
    animation

---
+++ FateChess/Assets/Scripts/CardFlyAnim.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFlyAnim : MonoBehaviour {
+
+	public GameObject[] Cards = new GameObject[4];
+	public Animator FlyOutCard;
+
+	private Vector3 StartFlyPosition = new Vector3 (800, 300, 0);
+	private Vector3 EndFlyPosition = new Vector3 (128-50, -128+50, 0);
+	private int cardnum;
+	private bool TurnStarting=false;
+	private float step=0;
+
+	void Update(){
+		if (Input.GetMouseButtonDown(0)) {
+			CardFlyIn (3);
+		}
+		if (TurnStarting) {
+			step += 2*Time.deltaTime;
+			Cards [cardnum].GetComponent<RectTransform> ().localPosition
+			= Vector3.Lerp (StartFlyPosition, EndFlyPosition, step);
+			if (step>=1) {
+				step = 0;
+				TurnStarting = false;
+			}
+		}
+	}
+
+	public void CardFlyIn(int CardNum){
+		Cards [CardNum].transform.parent.gameObject.SetActive (true);
+		Cards [CardNum].GetComponent<RectTransform> ().position = StartFlyPosition;
+		cardnum = CardNum;
+		TurnStarting = true;
+	}
+
+	public void CardFlyOut(int CardNum){
+		Cards [cardnum].GetComponent<RectTransform> ().position = EndFlyPosition;
+		step = 0;
+		TurnStarting = false;
+		Cards [CardNum].transform.parent.gameObject.SetActive (false);
+		FlyOutCard.Play ("FlyOut");
+	}
+}
