5a0ec9006c61e2c440c3d95c4d6d78be645f6fd9
Odls <alexeatapple@yahoo.com.tw>
2018-04-28 18:09:02 +0800

    擺放棋子

---
+++ FateChess/Assets/Scripts/Obj/PawnObj.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnObj : MonoBehaviour {
+	[LockInInspector]public int index = -1;
+	[NullAlarm]public SpriteRenderer imageRender, atkRender, hpRender;
+	PawnData _data = null;
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	public void SetPawnData (PawnData p_data) {
+		_data = p_data;
+		imageRender.sprite = _data.typeData.image;
+		atkRender.enabled = (p_data.atk >= 0);
+		hpRender.enabled = (p_data.hp >= 0);
+	}
+}
---
+++ FateChess/Assets/Scripts/Sys/PawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PawnManager : ManagerBase<PawnManager> {
+	public PawnObj pawnPrefab;
+	public PawnData pawnNull;
+	public List<PawnData> playerPawnDataList = new List<PawnData>();
+	public List<PawnData> enemyPawnDataList = new List<PawnData>();
+	[V2Lable("min", "max")]public Vector2 weekPawnRange;
+	[V2Lable("min", "max")]public Vector2 middlePawnRange;
+	[V2Lable("min", "max")]public Vector2 strongPawnRange;
+//	public override void Awake () {
+//
+//	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
