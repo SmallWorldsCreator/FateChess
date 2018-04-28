46d9251b46c6c10c42051ef13aa5cd0bb2869389
Odls <alexeatapple@yahoo.com.tw>
2018-04-29 07:23:08 +0800

    Merge branch 'master' of https://github.com/SmallWorldsCreator/FateChess

--- FateChess/Assets/Scripts/Sys/HeroObj.cs
+++ FateChess/Assets/Scripts/Sys/HeroObj.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroObj : MonoBehaviour {
 	[LockInInspector]public E_PawnSide side;
 
 	public List<FateData> fateList = new List<FateData>();
 	public List<PawnData> pawnList = new List<PawnData>();
+
+	public GameObject[] pawnCards = new GameObject[4];
+	public GameObject[] fateCards = new GameObject[3];
+	public GameObject usedCard;
+	public Sprite[] numbers = new Sprite[11];
 	int nowSelectIndex = -1;
 
 	const int maxValue = 10;
@@ -55,6 +61,7 @@ public class HeroObj : MonoBehaviour {
 
 
 		Refrash ();
+
 	}
 
 	public void AddCard(PawnData p_data){
@@ -79,7 +86,21 @@ public class HeroObj : MonoBehaviour {
 	}
 
 	public void UseCard(){
-		pawnList.RemoveAt (nowSelectIndex);
+		if (side == E_PawnSide.Player) {
+			GameManager.instance.CanvasAnimator.GetComponent<CardFlyAnim> ().CardFlyOut (nowSelectIndex);
+		}
+		int usedCount =0;
+		for (int i = 0; i < 4; i++) {
+			if (!pawnCards [i].transform.parent.gameObject.activeSelf) {
+				usedCount += 1;
+			}
+		}
+		Debug.Log (usedCount);
+		if (usedCount < nowSelectIndex) {
+			pawnList.RemoveAt (nowSelectIndex - usedCount);
+		} else {
+			pawnList.RemoveAt (nowSelectIndex);
+		}
 		Refrash ();
 	}
 
@@ -103,12 +124,48 @@ public class HeroObj : MonoBehaviour {
 	public void DrawCard(){
 		// @@@@@@
 //		fateList[f].probability
-
+		if (GameManager.instance.state != E_GAME_STATE.Init&& side == E_PawnSide.Player) {
+			GameManager.instance.CanvasAnimator.gameObject.GetComponent<CardFlyAnim> ().CardFlyIn (pawnList.Count);
+			GameManager.instance.CanvasAnimator.Play ("DrawCard");
+		}
 
 		AddCard (fateList[0].data);
 	}
 
 	public void Refrash(){
+		if (GameManager.instance.state != E_GAME_STATE.Init) {
+			for (int i = 0; i < 3; i++) {
+				fateCards [i].transform.GetChild (0).GetComponent<Image> ().sprite = fateList [i].data.typeData.image;
+				fateCards [i].transform.GetChild (1).GetComponent<Text> ().text = fateList [i].probability.ToString () + "%";
+				fateCards [i].transform.GetChild (2).GetComponent<Image> ().sprite = numbers [fateList [i].data.hp];
+				fateCards [i].transform.GetChild (3).GetComponent<Image> ().sprite = numbers [fateList [i].data.atk];
+				fateCards [i].transform.GetChild (4).GetComponent<Image> ().sprite = numbers [fateList [i].data.cost];
+
+			}
+		}
+		if (GameManager.instance.state != E_GAME_STATE.Init && side == E_PawnSide.Player) {
+			int len = pawnList.Count;
+			for (int i = 0; i < len; i++) {
+				pawnCards [i].transform.GetChild (0).GetComponent<Image> ().sprite = pawnList [i].typeData.image;
+				pawnCards [i].transform.GetChild (2).GetComponent<Image> ().sprite = numbers[pawnList[i].cost];
+				pawnCards [i].transform.GetChild (3).GetComponent<Image> ().sprite = numbers[pawnList[i].atk];
+				pawnCards [i].transform.GetChild (4).GetComponent<Image> ().sprite = numbers[pawnList[i].hp];
+				pawnCards [i].transform.GetChild (5).GetComponent<Text> ().text = pawnList [i].typeData.name;
+				pawnCards [i].transform.parent.gameObject.SetActive (true);
+			}
+			for (int i = len; i < 4; i++) {
+				pawnCards [i].transform.parent.gameObject.SetActive (false);
+
+			}
+		}
+		if (nowSelectIndex > -1 && pawnCards[nowSelectIndex].activeSelf && side == E_PawnSide.Player) {
+			usedCard.transform.GetChild (0).GetComponent<Image> ().sprite = pawnCards [nowSelectIndex].transform.GetChild (0).GetComponent<Image> ().sprite;
+			usedCard.transform.GetChild (2).GetComponent<Image> ().sprite = pawnCards [nowSelectIndex].transform.GetChild (2).GetComponent<Image> ().sprite;
+			usedCard.transform.GetChild (3).GetComponent<Image> ().sprite = pawnCards [nowSelectIndex].transform.GetChild (3).GetComponent<Image> ().sprite;
+			usedCard.transform.GetChild (4).GetComponent<Image> ().sprite = pawnCards [nowSelectIndex].transform.GetChild (4).GetComponent<Image> ().sprite;
+			usedCard.transform.GetChild (5).GetComponent<Text> ().text = pawnCards [nowSelectIndex].transform.GetChild (5).GetComponent<Text> ().text;
+
+		}
 
 	}
 }
